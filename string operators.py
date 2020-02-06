36c6f8c773a92eda50a76b342a72458d79e881cd
studyfree20 <studyfree20@gmail.com>
2020-02-05 22:41:08 -0800

    Python Strings

---
+++ string operators.py
@@ -0,0 +1,15 @@
+'''
+string operators
++*r% format() in not in
+'''
+#start of string check
+a="Zetech"
+b="University"
+print(a+b)              #combines two strings/concatenation
+print(a*4)              #repeats a string number of times indicated
+print("hi{}".format(b))
+print('h' in a)         #returns true or false based on boolean
+print('n' not in a)     #returns true or false based on boolean
+print('x'in a)          #checking is a char is in a string and returns T OR F
+print('h' not in a) 
+#End of string check
