dfb32c42ee5d77ab2e2932ea432a5cc5a86b4c4b
studyfree20 <studyfree20@gmail.com>
2020-02-05 22:35:41 -0800

    Python Strings

---
+++ strings.py
@@ -0,0 +1,37 @@
+'''
+mystring
+string indexing
+string splittng
+string built in methods()
+repo name Python Strings
+'''
+a="Hello World!"
+print (a)
+#space is included and stats from 0
+print(a[0])
+print(a[8])
+print(a[-8])
+print(a[:])
+print(a[2:5])               #2 is included but 5 is not
+print(a[8:])
+print(a[:8])
+print(a[::])
+print(a[-1])
+print(a[-2])
+print(a[-3])
+#- reads chars from end of string with a -1
+myuni="Zetech University"
+print(myuni)
+                                #space is included and stats from 0
+print(myuni[0])
+print(myuni[8])
+print(myuni[-8])
+print(myuni[:])
+print(myuni[2:5])               #2 is included but 5 is not
+print(myuni[8:])
+print(myuni[:8])
+print(myuni[::])
+print(myuni[-3])
+                                #- reads chars from end of string with a -1
+
+
